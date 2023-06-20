d16116b7ccad4011312f78adba227c649d36aa6c
ALFKINN <tema16@bk.ru>
2023-06-20 10:58:27 +0300

    первое задание выполнено

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result =Convert.ToInt32(value);
+    return result;
+}
+
+int number = Prompt("Введите трехзначное число > ");
+if (number < 100 || number >= 1000)
+{
+    Console.WriteLine("Вы ввели не трехзначное число, повторите ввод");
+  return;
+}
+Console.WriteLine($"Введеное число {number}");
+int secondRank = number / 10 % 10;
+Console.WriteLine($"Вторая цифра {secondRank}");
